5fb1ed18b31b37d167686a87bc4f9b1189f7d609
Morten Lund Schmidt <mls@passwd.dk>
2016-12-27 22:42:52 +0100

    Changed `pathogen.vim` setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Creates directories used for vim backup
-directory=( "backup" "swp" "undodir" )
+directory=("autoload" "backup" "swp" "undodir" )
 for element in "${directory[@]}"
 do
     if [ ! -d "$element" ]; then
@@ -9,12 +9,15 @@ do
     fi
 done
 
+# Get pathogen.vim
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 # Initiate the git submodules
 git submodule init
 git submodule update
 
 # Create symbolic link to vims package manager
-ln -s ~/.vim/bundle/vim-pathogen/autoload ~/.vim/
+#ln -s ~/.vim/bundle/vim-pathogen/autoload ~/.vim/
 
 # Create symbolic link to vim
 ln -s ~/.vim/vimrc ~/.vimrc
